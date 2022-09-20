3dac4a333989dcf7dfb4e498096124c029f48595
Vlad Gervald <isfreetime@ya.ru>
2022-09-21 00:17:02 +0300

    add program

--- Speed List/Speed List.cpp
+++ Speed List/Speed List.cpp
@@ -2,19 +2,54 @@
 //
 
 #include <iostream>
+#include <fstream>
+#include <cmath>
+
+
+#define PI 3.141592653589793
+
+
+
+using namespace std;
+
+double Vx = 0;
+double Vy = 0;
+double W = 0;
+
+
 
 int main()
 {
-    std::cout << "Hello World!";
-}
+    ofstream fout;
+    fout.open("Coords2.txt");
 
-// Запуск программы: CTRL+F5 или меню "Отладка" > "Запуск без отладки"
-// Отладка программы: F5 или меню "Отладка" > "Запустить отладку"
+    double stepTime = 0.1;
+    double totalTime = 15; 
+    double Velocity = 0.15;
 
-// Советы по началу работы 
-//   1. В окне обозревателя решений можно добавлять файлы и управлять ими.
-//   2. В окне Team Explorer можно подключиться к системе управления версиями.
-//   3. В окне "Выходные данные" можно просматривать выходные данные сборки и другие сообщения.
-//   4. В окне "Список ошибок" можно просматривать ошибки.
-//   5. Последовательно выберите пункты меню "Проект" > "Добавить новый элемент", чтобы создать файлы кода, или "Проект" > "Добавить существующий элемент", чтобы добавить в проект существующие файлы кода.
-//   6. Чтобы снова открыть этот проект позже, выберите пункты меню "Файл" > "Открыть" > "Проект" и выберите SLN-файл.
+    int quantityPoint = totalTime / stepTime;
+    int lenght2R = quantityPoint / (PI + 1);
+    int lenghtL = (PI * quantityPoint) / (PI + 1);
+
+    double gamma;
+
+    cout << quantityPoint << "\t" << lenght2R << "\t" << lenghtL;
+
+    for (int i = 0; i < quantityPoint; i++)
+    {
+        if (i <= lenght2R) {
+            Vx = Velocity;
+            fout << Vx << " " << Vy << " ";
+        }
+        else if (i > lenght2R) {
+            gamma = PI / lenghtL * (i - lenght2R);
+            Vx = Velocity * sin(gamma);
+            Vy = Velocity * cos(gamma);
+            fout << Vx << " " << Vy << " ";
+        
+        }
+    }
+
+    fout.close();
+    
+ }
