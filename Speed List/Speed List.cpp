9efee049fe465825c2fa9e5ded14e672e6c60564
Vlad Gervald <isfreetime@ya.ru>
2022-09-21 19:54:08 +0300

    change circle and add roll

--- Speed List/Speed List.cpp
+++ Speed List/Speed List.cpp
@@ -5,10 +5,7 @@
 #include <fstream>
 #include <cmath>
 
-
 #define PI 3.141592653589793
-
-
 
 using namespace std;
 
@@ -16,12 +13,10 @@ double Vx = 0;
 double Vy = 0;
 double W = 0;
 
-
-
 int main()
 {
     ofstream fout;
-    fout.open("Coords2.txt");
+    fout.open("SpeedListW.txt");
 
     double stepTime = 0.1;
     double totalTime = 15; 
@@ -35,20 +30,37 @@ int main()
 
     cout << quantityPoint << "\t" << lenght2R << "\t" << lenghtL;
 
+/*
     for (int i = 0; i < quantityPoint; i++)
     {
         if (i <= lenght2R) {
             Vx = Velocity;
-            fout << Vx << " " << Vy << " ";
+            fout << Vx << " " << Vy << " " << W << endl;
         }
         else if (i > lenght2R) {
             gamma = PI / lenghtL * (i - lenght2R);
-            Vx = Velocity * sin(gamma);
-            Vy = Velocity * cos(gamma);
-            fout << Vx << " " << Vy << " ";
+            Vx = -Velocity * sin(gamma);
+            Vy = -Velocity * cos(gamma);
+            fout << Vx << " " << Vy << " " << W << endl;
         
         }
     }
+    
+    */
+
+    
+
+    for (int i = 0; i < quantityPoint; i++)
+    {
+        if (i < quantityPoint / 2) {
+            W = Velocity;
+        }
+        else if (i >= quantityPoint / 2) {
+            W = -Velocity;
+        }
+        fout << Vx << " " << Vy << " " << W << endl;
+    }
+
 
     fout.close();
     
